be3d5a3ba751e85d4d1324ef1a6f1c77a986fc3a
euno <seal5945@gmail.com>
2022-09-08 20:12:29 +0900

    a

--- 1002.py
+++ 1002.py
@@ -2,13 +2,14 @@ import sys
 
 T = int(input())
 for _ in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
-    if abs(r1 - r2) < (x1 - x2) ** 2 + (y1 - y2) ** 2 < r1 + r2:
-        print(0)
-    elif (x1 - x2) ** 2 + (y1 - y2) ** 2 == r1 + r2 \
-            or abs(r1 - r2) == (x1 - x2) ** 2 + (y1 - y2) ** 2:
-        print(1)
-    elif (x1 - x2) ** 2 + (y1 - y2) ** 2 > r1 + r2:
+    x1, y1, r1, x2, y2, r2 = map(float, sys.stdin.readline().split())
+    d = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1/2)
+
+    if abs(r2 - r1) < d < r1 + r2:
         print(2)
-    elif x1 == x2 and y1 == y2 and r1 == r2:
+    elif d == 0 and r1 == r2:
         print(-1)
+    elif d == r1 + r2 or d == abs(r1 - r2):
+        print(1)
+    else:
+        print(0)
