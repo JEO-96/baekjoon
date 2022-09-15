60793d7604468fbc1be17af0ed6c0d8a9f1ea838
euno <seal5945@gmail.com>
2022-09-15 21:36:46 +0900

    update

--- 9375.py
+++ 9375.py
@@ -1,15 +1,18 @@
 import sys
 import collections
 
+
 T = int(sys.stdin.readline())
 for _ in range(T):
+    wearing = collections.defaultdict(int)
     n = int(sys.stdin.readline())
     names = []
     result = 1
     for i in range(n):
         A, B = sys.stdin.readline().split()
-        names.append(B)
-    num = collections.Counter(names)
-    for key in num:
-        result *= num[key] + 1
+        if B not in names:
+            names.append(B)
+        wearing[B] += 1
+    for j in names:
+        result *= wearing[j] + 1
     print(result - 1)
