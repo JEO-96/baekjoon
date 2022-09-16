4e53616832d08c38ef9b149393ee03154bc06204
euno <seal5945@gmail.com>
2022-09-16 16:43:35 +0900

    update

---
+++ 15651.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+s = []
+
+
+def n_m():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, N + 1):
+            s.append(i)
+            n_m()
+            s.pop()
+
+
+n_m()
